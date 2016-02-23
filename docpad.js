d0073a281b82888f94cc78aa96675d1689dd7c35
Vanessa Me Tonini <vanessametonini@gmail.com>
2016-02-23 13:29:14 -0300

    Atualizado agenda e infos das palestrantes

--- docpad.js
+++ docpad.js
@@ -87,7 +87,7 @@ module.exports = {
       {
         name: "Gedeane Kenshima",
         //photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "",
+        bio: "Gedeane Kenshima é engenheira de controle e automação, técnica em automação industria, ambos pelo IFSP. Trabalha como montadora na Tudela Indústria. Hobbista e maker, vem desenvolvendo projetos com arduíno em 2013 e wearables desde 2015. Os eletrônicos desenvolvidos são voltados para a área de moda e entretenimento.",
         company: "",
         // link: {
         //   href: "http://github.com/billy95",
@@ -95,29 +95,29 @@ module.exports = {
         // },
         presentation: {
           title: "Arduíno para meninas",
-          description: "[Sala 3T] Workshop de arduíno. Duração: 1h 30m.",
+          description: "[Sala 3T] Nesse workshop serão apresentados conceitos básicos de programação voltado à experimentações em arduíno. A atividade é voltada para meninas e adolescentes a partir de 9 anos (8 vagas). Duração: 1h 30m.",
           time: "10h30"
         }
       },
       {
         name: "Priscila Zillo",
         photo: "themes/yellow-swan/img/priscila.jpg",
-        bio: "",
-        company: "",
+        bio: "Priscila Amaral Zillo é CEO do Curso Sapientia, empresa de e-learning que prepara diplomatas para concurso público. O Sapientia tem investido em tecnologia e soluções criativas para crescer em um mercado em crise.",
+        company: "Sapientia",
         // link: {
         //   href: "http://github.com/billy95",
         //   text: "@billy95"
         // },
         presentation: {
           title: "Tecnologia e mudanças na organização do trabalho",
-          description: "[Sala 1A] Duração: 1h.",
+          description: "[Sala 1A] A Revolução Industrial foi uma revolução lenta e, de certa maneira, silenciosa, mas com profundos impactos sociais e na organização do trabalho. Alguns especialistas identificam a nossa época com a Quarta Revolução Industrial, muito mais rápida comparada às outras 3 em termos do ritmo de mudanças. A CEO do Curso Sapientia fala sobre inovação na área de e-learning tendo como mercado um público-alvo conservador (ou seja, resistente à tecnologia), de projetos de tecnologia na empresa e do uso das redes sociais para promoção do trabalho. O trabalho à frente do Sapientia é um caso de estudo para falar de mudanças mais amplas na organização do trabalho. Duração: 1h.",
           time: "10h30"
         }
       },
       {
         name: "Amanda Pinheiro",
         photo: "themes/yellow-swan/img/amanda.jpg",
-        bio: "",
+        bio: "Amanda Gomes Pinheiro é fundadora da AMG Inovação, Comunicação e Desenvolvimento, empresa que atua no desenvolvimento de empreendedores e líderes em todo o Brasil.Formada em Administração de Empresas, com pós-graduacão em Varejo pela Fia USP e MBA em Estratégia Empresarial, é tambeem especialista em comportamento humano e coach. Abriu sua primeira empresa aos 21 anos e liderou funções estratégicas ao longo de 18 anos em grandes empresas de segmentos como tecnologia da informação, telecom, indústria, finanças e varejo.",
         company: "",
         // link: {
         //   href: "http://github.com/billy95",
@@ -125,14 +125,14 @@ module.exports = {
         // },
         presentation: {
           title: "Tecnologia e empreendedorismo",
-          description: "[Sala 1T] Como unir as duas coisas para ter sucesso? Duração: 1h.",
+          description: "[Sala 1T] Orientação a mulheres que querem assumir os desafios de empreender e liderar com mais assertividade, segurança e êxito. Duração: 1h.",
           time: "11h30"
         }
       },
       {
         name: "Malu Andrade",
         //photo: "themes/yellow-swan/img/amanda.jpg",
-        bio: "",
+        bio: "Malu Andrade é coordenadora de Inovação da Spcine. Mestranda em arquivos audiovisuais em plataformas digitais pela USP e especialização em Estéticas Tecnológicas pela PUC. Trabalha com gestão cultural há 10 anos, tendo se dedicado desde 2010 a cultura digital.",
         company: "SP Cine (WUG)",
         // link: {
         //   href: "http://github.com/billy95",
@@ -140,7 +140,7 @@ module.exports = {
         // },
         presentation: {
           title: "Políticas públicas e gênero feminino nos games",
-          description: "[Sala 1A] Como unir as duas coisas para ter sucesso? Duração: 30m.",
+          description: "[Sala 1A] A palestra abordará os seguintes temas: Política de inovação da SPcine - LEIA e política para games. Políticas públicas para games no Brasil. Participação da mulher no audiovisual no país. Duração: 30m.",
           time: "11h00"
         }
       },
@@ -151,7 +151,7 @@ module.exports = {
       {
         name: "Ariane Parra",
         //photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "",
+        bio: "Ariane Parra da Women Up Games e Guz Forster da Experimentoria",
         company: "Women Up Games",
         // link: {
         //   href: "http://twitter.com/littlechuck",
@@ -159,14 +159,14 @@ module.exports = {
         // },
         presentation: {
           title: "Competição de Games (WUP)",
-          description: "[Sala 2T] Duração: 1h.",
+          description: "[Sala 2T] Quer se juntar a um monte de mulheres e jogar aquele game da infância e adolescência no fliperama? Então participe da competição de Street Fighter só para elas. Venha conhecer nosso arcade inteiramente montado por nós e saber das nossas Oficinas. Duração: 1h.",
           time: "13h30"
         }
       },
       {
-        name: "",
+        name: "Bi Aguilart",
         //photo: "themes/yellow-swan/img/speaker.jpg",
-        //bio: "",
+        bio: "Ilustradora em parceria com a Women Up Games.",
         company: "Women Up Games",
         // link: {
         //   href: "http://github.com/stevie",
@@ -174,14 +174,14 @@ module.exports = {
         // },
         presentation: {
           title: "Ilustração de Games",
-          description: "[Sala 1T] Duração: 1h 30m.",
+          description: "[Sala 1T] #Empoderadas é o nome desse workshop. Personagens femininas de games que foram criadas de forma sexualizada ou como \"donzelas\" serão desenhadas e transformadas, pela artista e ilustradora Bi Aguilart, em figuras humanizadas e empoderadas. Duração: 1h 30m.",
           time: "15h00"
         }
       },
       {
         name: "Vanessa Tonini",
         photo: "themes/yellow-swan/img/vanessa.jpg",
-        bio: "Agile Coach na Lambda3. Voluntária na Marialab",
+        bio: "Agile Coach na Lambda 3, trabalhou como desenvolvedora frontend nos últimos 7 anos. Formada em Sistemas para Internet pela UNIVALI, Especialista em Desenvolvimento Web pela UTFPR e Engenheira de Software pela FIAP, atuou em diferentes segmentos dentro do desenvolvimento frontend, como: ecommerce, acessibilidade e web design responsivo. Também faz parte da organização da Marialab Hackerspace, onde trabalha para a causa das mulheres na tecnologia e inclusão digital.",
         company: "Marialab",
         link: {
           href: "http://github.com/vanessametonini",
@@ -196,15 +196,15 @@ module.exports = {
       {
         name: "Tatiana Barros",
         photo: "themes/yellow-swan/img/taty-barros.jpg",
-        //bio: "",
+        bio: "Tatiana Barros, mãe e empreendedora. Criadora da Saga Pólen, iniciativa de temática ambiental que promove o desenvolvimento de habilidades multidisciplinares em crianças do ensino fundamental.",
         company: "Saga Pólen",
         // link: {
         //   href: "http://github.com/stevie",
         //   text: "@stevie"
         // },
         presentation: {
-          title: "Workshop de Web design",
-          description: "[Sala 1A] Tatiana Barros. Duração: 30m.",
+          title: "Tatiana Barros",
+          description: "[Sala 1A] Apresentação focada nas escolhas profissionais com base no sistema de ensino atual. Como as meninas são preparadas e inspiradas e qual a disruptura no mercado quando ela cresce serão tópicos abordados. Duração: 30m.",
           time: "15h00"
         }
       },
